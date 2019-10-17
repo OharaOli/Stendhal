1c011d46a08347abd0a929fc6bdb07429976b369
Wolodymyr Krywonos <wolodymyr.krywonos@student.manchester.ac.uk>
2019-10-17 10:37:27 +0100

    add test and fixed clasp bug

---
+++ tests/games/stendhal/server/entity/item/MithrilClaspTest.java
@@ -0,0 +1,24 @@
+package games.stendhal.server.entity.item;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.maps.MockStendlRPWorld;
+
+public class MithrilClaspTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		MockStendlRPWorld.get();
+	}
+	
+	@Test
+	public void test() {
+		final Item clasp = SingletonRepository.getEntityManager().getItem("mithril clasp");
+		assertTrue(clasp.canBeEquippedIn("keyring"));
+	}
+
+}
