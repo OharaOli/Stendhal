587430ed0cfb2d15b2553f9f1f8740520f12ac24
Armand Anusca <ioan.anuscapopa@student.manchester.ac.uk>
2019-11-14 08:47:16 +0000

    Implemented first release of Lon Jatham NPC
    Updated the Lon Jatham test suite with new responses and comments
    Created Lon Jatham NPC
    When asked, Lon can answer all the questions needed in order to recruit
    the other NPCs
    Added custom graphics for Lon Jatham
    Placed Lon in Deniran

--- tests/games/stendhal/server/maps/deniran/LonJathamNPCTest.java
+++ tests/games/stendhal/server/maps/deniran/LonJathamNPCTest.java
@@ -13,10 +13,15 @@ import games.stendhal.server.entity.npc.fsm.Engine;
 import utilities.QuestHelper;
 import utilities.ZonePlayerAndNPCTestImpl;
 
-
+/**
+ * Test suite for the Lon Jatham NPC
+ * It tests every specific functionality of this NPC
+ * @author armandanusca
+ *
+ */
 public class LonJathamNPCTest extends ZonePlayerAndNPCTestImpl{
 	
-	private static final String ZONE_NAME = "int_deniram";
+	private static final String ZONE_NAME = "0_deniran";
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -35,41 +40,46 @@ public class LonJathamNPCTest extends ZonePlayerAndNPCTestImpl{
 		final SpeakerNPC npc = getNPC("Lon Jatham");
 		final Engine en = npc.getEngine();
 
-		assertTrue(en.step(player, "hi Lon Jatham"));
-		assertEquals("Good afternoon! Welcome to the DIT School of Computer Science", getReply(npc));
+		assertTrue(en.step(player, "Hi"));
+		assertEquals("Good morning ! Are you ready for a lecture ?", getReply(npc));
 
-		assertTrue(en.step(player, "bye"));
-		assertEquals("See you at the next lecture", getReply(npc));
+		assertTrue(en.step(player, "Bye"));
+		assertEquals("That's all for today, see you next time, whenewer that will be !", getReply(npc));
 	}
-	/**test if lon jatham answers the required questions right */
+	
+	/**
+	 * Tests if Lon can answers all the questions needed
+	 */
 	@Test
 	public void testQuestions() {
+		
+		// Getting the NPC
 		final SpeakerNPC npc = getNPC("Lon Jatham");
 		final Engine en = npc.getEngine();
 
-		assertTrue(en.step(player, "hi Lon Jatham"));
-		assertEquals("Good afternoon! Welcome to the DIT School of Computer Science", getReply(npc));
+		assertTrue(en.step(player, "Hi"));
+		assertEquals("Good morning ! Are you ready for a lecture ?", getReply(npc));
 		
-		assertTrue(en.step(player, "question"));
-		assertEquals("Ask me about the School of Computer Science", getReply(npc));
+		assertTrue(en.step(player, "Question"));
+		assertEquals("Ask me about the School of Computer Science.", getReply(npc));
 		
 		assertTrue(en.step(player, "Python"));
 		assertEquals("You will learn and use Python for different courses, including 1st year Fundamentals of AI and 2nd year Machine Learning ", getReply(npc));
 		
-		assertTrue(en.step(player, "question"));
-		assertEquals("Ask me about the School of Computer Science", getReply(npc));
+		assertTrue(en.step(player, "Question"));
+		assertEquals("Ask me about the School of Computer Science.", getReply(npc));
 		
 		assertTrue(en.step(player, "AI"));
 		assertEquals("In the first year you will study the Fundamentals of AI, while in the 2nd year you can choose Symbolic AI and Machine Learning as optional courses", getReply(npc));
 		
 		assertTrue(en.step(player, "question"));
-		assertEquals("Ask me about the School of Computer Science", getReply(npc));
+		assertEquals("Ask me about the School of Computer Science.", getReply(npc));
 		
 		assertTrue(en.step(player, "Computer Science with Business Management"));
 		assertEquals("We no longer have a Computer Science with Business Management programme", getReply(npc));
 		
-		assertTrue(en.step(player, "bye"));
-		assertEquals("See you at the next lecture", getReply(npc));
+		assertTrue(en.step(player, "Bye"));
+		assertEquals("That's all for today, see you next time, whenewer that will be !", getReply(npc));
 			
 
 	}
